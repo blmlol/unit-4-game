53f02cfb0eaba7eaceb63876d1ff90de56aa6dd4
Davey Wong <notwdjw@gmail.com>
2019-11-02 20:48:47 -0700

    Fixed multiple clicking functionality along with display of statlines for each character while in attack or defense mode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,28 +6,32 @@ var chars = {
         hp: 500,
         ap: 15,
         cap: 18,
-        isClicked: false
+        isAttacking: false,
+        isDefending: false
     },
 
     grievous: {
         hp: 300,
         ap: 10,
         cap: 30,
-        isClicked: false
+        isAttacking: false,
+        isDefending: false
     },
 
     rey: {
         hp: 250,
         ap: 18,
         cap: 12,
-        isClicked: false
+        isAttacking: false,
+        isDefending: false
     },
 
     kylo: {
         hp: 400,
         ap: 8,
         cap: 20,
-        isClicked: false
+        isAttacking: false,
+        isDefending: false
     }
 
 
@@ -38,49 +42,95 @@ var chars = {
 $(document).ready(function () {
     //These hover functions will show the statline of each character (Their HP, AP, and CAP) when hovered over
     $('.gg').hover(function () {
-        $('.ggcap').append($('<div>' + 'HP: ' + chars.grievous.hp + '<br>' + 'AP: ' + chars.grievous.ap + '<br>' + 'CAP: ' + chars.grievous.cap + '</div>'));
+        if (chars.grievous.isDefending !== true) {
+            $('.ggcap').append($('<div>' + 'HP: ' + chars.grievous.hp + '<br>' + 'AP: ' + chars.grievous.ap + '<br>' + 'CAP: ' + chars.grievous.cap + '</div>'));
+        }
+
     }, function () {
         $(this).find('div').last().remove();
     })
 
     $('.kr').hover(function () {
-        $('.krcap').append($('<div>' + 'HP: ' + chars.kylo.hp + '<br>' + 'AP: ' + chars.kylo.ap + '<br>' + 'CAP: ' + chars.kylo.cap + '</div>'));
+        if (chars.kylo.isDefending !== true) {
+            $('.krcap').append($('<div>' + 'HP: ' + chars.kylo.hp + '<br>' + 'AP: ' + chars.kylo.ap + '<br>' + 'CAP: ' + chars.kylo.cap + '</div>'));
+        }
+
     }, function () {
         $(this).find('div').last().remove();
     })
 
     $('.re').hover(function () {
-        $('.recap').append($('<div>' + 'HP: ' + chars.rey.hp + '<br>' + 'AP: ' + chars.rey.ap + '<br>' + 'CAP: ' + chars.rey.cap + '</div>'));
+        if (chars.rey.isDefending !== true) {
+            $('.recap').append($('<div>' + 'HP: ' + chars.rey.hp + '<br>' + 'AP: ' + chars.rey.ap + '<br>' + 'CAP: ' + chars.rey.cap + '</div>'));
+        }
+
     }, function () {
         $(this).find('div').last().remove();
     })
 
     $('.ow').hover(function () {
-        $('.owcap').append($('<div>' + 'HP: ' + chars.obiwan.hp + '<br>' + 'AP: ' + chars.obiwan.ap + '<br>' + 'CAP: ' + chars.obiwan.cap + '</div>'));
+        if (chars.obiwan.isDefending !== true) {
+            $('.owcap').append($('<div>' + 'HP: ' + chars.obiwan.hp + '<br>' + 'AP: ' + chars.obiwan.ap + '<br>' + 'CAP: ' + chars.obiwan.cap + '</div>'));
+        }
+
     }, function () {
         $(this).find('div').last().remove();
     })
 
     //Next we will want a click event listener to allow the user to choose a character and put chosen character in attacker div
-    $('.gg').on('click', function () {
-        $('.attacker').append($(this));
-        chars.grievous.isClicked = true;
-        $('.defender').append($('.kr'), $('.ow'), $('.re'));
+    $('.gg').one('click', function () {
+        if (chars.grievous.isDefending !== true) {
+
+
+            $('.attacker').append($(this));
+            chars.grievous.isAttacking = true;
+            $('.defender').append($('.kr'), $('.ow'), $('.re'));
+            $('#defstats').append($('<div class="stats">' + 'HP: ' + chars.kylo.hp + '<br>' + 'AP: ' + chars.kylo.ap + '<br>' + 'CAP: ' + chars.kylo.cap + '</div>'), $('<div class="stats">' + 'HP: ' + chars.obiwan.hp + '<br>' + 'AP: ' + chars.obiwan.ap + '<br>' + 'CAP: ' + chars.obiwan.cap + '</div>'), $('<div class="stats">' + 'HP: ' + chars.rey.hp + '<br>' + 'AP: ' + chars.rey.ap + '<br>' + 'CAP: ' + chars.rey.cap + '</div>').html());
+            $('.warstats').append('<div>' + 'HP: ' + chars.grievous.hp + '<br>' + 'AP: ' + chars.grievous.ap + '<br>' + 'CAP: ' + chars.grievous.cap + '</div>');
+            chars.obiwan.isDefending = true;
+            chars.kylo.isDefending = true;
+            chars.rey.isDefending = true;
+
+        }
     })
 
-    $('.kr').on('click', function () {
-        $('.attacker').append($(this));
-        chars.kylo.isClicked = true;
+    $('.kr').one('click', function () {
+        if (chars.kylo.isDefending !== true) {
+            $('.attacker').append($(this));
+            chars.kylo.isAttacking = true;
+            $('.defender').append($('.gg'), $('.ow'), $('.re'));
+            $('#defstats').append($('<div class="stats">' + 'HP: ' + chars.grievous.hp + '<br>' + 'AP: ' + chars.grievous.ap + '<br>' + 'CAP: ' + chars.grievous.cap + '</div>'), $('<div class="stats">' + 'HP: ' + chars.obiwan.hp + '<br>' + 'AP: ' + chars.obiwan.ap + '<br>' + 'CAP: ' + chars.obiwan.cap + '</div>'), $('<div class="stats">' + 'HP: ' + chars.rey.hp + '<br>' + 'AP: ' + chars.rey.ap + '<br>' + 'CAP: ' + chars.rey.cap + '</div>').html());
+            $('.warstats').append('<div>' + 'HP: ' + chars.kylo.hp + '<br>' + 'AP: ' + chars.kylo.ap + '<br>' + 'CAP: ' + chars.kylo.cap + '</div>');
+            chars.obiwan.isDefending = true;
+            chars.grievous.isDefending = true;
+            chars.rey.isDefending = true;
+        }
     })
 
-    $('.ow').on('click', function () {
-        $('.attacker').append($(this));
-        chars.obiwan.isClicked = true;
+    $('.ow').one('click', function () {
+        if (chars.obiwan.isDefending !== true) {
+            $('.attacker').append($(this));
+            chars.obiwan.isAttacking = true;
+            $('.defender').append($('.kr'), $('.gg'), $('.re'));
+            $('#defstats').append($('<div class="stats">' + 'HP: ' + chars.kylo.hp + '<br>' + 'AP: ' + chars.kylo.ap + '<br>' + 'CAP: ' + chars.kylo.cap + '</div>'), $('<div class="stats">' + 'HP: ' + chars.grievous.hp + '<br>' + 'AP: ' + chars.grievous.ap + '<br>' + 'CAP: ' + chars.grievous.cap + '</div>'), $('<div class="stats">' + 'HP: ' + chars.rey.hp + '<br>' + 'AP: ' + chars.rey.ap + '<br>' + 'CAP: ' + chars.rey.cap + '</div>').html());
+            $('.warstats').append('<div>' + 'HP: ' + chars.obiwan.hp + '<br>' + 'AP: ' + chars.obiwan.ap + '<br>' + 'CAP: ' + chars.obiwan.cap + '</div>');
+            chars.grievous.isDefending = true;
+            chars.kylo.isDefending = true;
+            chars.rey.isDefending = true;
+        }
     })
 
-    $('.re').on('click', function () {
-        $('.attacker').append($(this));
-        chars.rey.isClicked = true;
+    $('.re').one('click', function () {
+        if (chars.rey.isDefending !== true) {
+            $('.attacker').append($(this));
+            chars.rey.isAttacking = true;
+            $('.defender').append($('.kr'), $('.ow'), $('.gg'));
+            $('#defstats').append($('<div class="stats">' + 'HP: ' + chars.kylo.hp + '<br>' + 'AP: ' + chars.kylo.ap + '<br>' + 'CAP: ' + chars.kylo.cap + '</div>'), $('<div class="stats">' + 'HP: ' + chars.obiwan.hp + '<br>' + 'AP: ' + chars.obiwan.ap + '<br>' + 'CAP: ' + chars.obiwan.cap + '</div>'), $('<div class="stats">' + 'HP: ' + chars.grievous.hp + '<br>' + 'AP: ' + chars.grievous.ap + '<br>' + 'CAP: ' + chars.grievous.cap + '</div>').html());
+            $('.warstats').append('<div>' + 'HP: ' + chars.rey.hp + '<br>' + 'AP: ' + chars.rey.ap + '<br>' + 'CAP: ' + chars.rey.cap + '</div>');
+            chars.obiwan.isDefending = true;
+            chars.kylo.isDefending = true;
+            chars.grievous.isDefending = true;
+        }
     })
 });
 
